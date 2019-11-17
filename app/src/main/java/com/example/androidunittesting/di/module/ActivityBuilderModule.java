59257e01a904db04a212c65cfa7fb2c0026b07bf
imtiaz <programmer.imtiaz@gmail.com>
2019-11-17 19:49:41 +0600

    ADD- NoteListActivity injected into in ActivityBuilderModule

--- app/src/main/java/com/example/androidunittesting/di/module/ActivityBuilderModule.java
+++ app/src/main/java/com/example/androidunittesting/di/module/ActivityBuilderModule.java
@@ -1,9 +1,13 @@
 package com.example.androidunittesting.di.module;
 
+import com.example.androidunittesting.ui.activity.NotesListActivity;
+
 import dagger.Module;
+import dagger.android.ContributesAndroidInjector;
 
 @Module
 public abstract class ActivityBuilderModule {
 
-
+    @ContributesAndroidInjector
+    abstract NotesListActivity contributeNoteListActivity();
 }
