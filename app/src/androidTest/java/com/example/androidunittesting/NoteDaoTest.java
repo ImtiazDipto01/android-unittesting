45200b247992f448d012ad58e2e8b76863aad46e
imtiaz <programmer.imtiaz@gmail.com>
2019-11-30 09:39:01 +0600

    ADD- second instrumented test written on Insert, read, update, read, delete

--- app/src/androidTest/java/com/example/androidunittesting/NoteDaoTest.java
+++ app/src/androidTest/java/com/example/androidunittesting/NoteDaoTest.java
@@ -11,6 +11,10 @@ import java.util.List;
 
 public class NoteDaoTest extends MyDatabaseManagerTest {
 
+    public static final String TEST_TITLE = "This is a test Title";
+    public static final String TEST_CONTENT = "This is a test Content";
+    public static final String TEST_TIMESTAMP = "01-2019";
+
     // Insert, Read, Delete
     @Test
     public void testInsertReadDelete() throws Exception{
@@ -37,5 +41,51 @@ public class NoteDaoTest extends MyDatabaseManagerTest {
         Assert.assertEquals(0, insertedList.size());
 
     }
+
+    //Insert read update read delete
+    @Test
+    public void testInsertReadUpdateReadDelete() throws Exception{
+        Note note = new Note(TestUtil.TEST_NOTE_1);
+
+        //Insert
+        getNoteDao().insertNote(note).blockingGet(); //wait until inserted because we are using rxJava
+
+        //read
+        LiveDataTestUtil<List<Note>> liveDataTestUtilList = new LiveDataTestUtil<>();
+        List<Note> insertedList = liveDataTestUtilList.getValue(getNoteDao().getNoteList());
+
+        Assert.assertNotNull(insertedList);
+
+        Assert.assertEquals(note.getContent(), insertedList.get(0).getContent());
+        Assert.assertEquals(note.getTimestamp(), insertedList.get(0).getTimestamp());
+        Assert.assertEquals(note.getTitle(), insertedList.get(0).getTitle());
+
+        note.setId(insertedList.get(0).getId());
+        Assert.assertEquals(note, insertedList.get(0));
+
+        //update
+        note.setTitle(TEST_TITLE);
+        note.setTimestamp(TEST_TIMESTAMP);
+        note.setContent(TEST_CONTENT);
+        getNoteDao().updateNote(note).blockingGet();
+
+        //read
+        insertedList = liveDataTestUtilList.getValue(getNoteDao().getNoteList());
+        Assert.assertEquals(TEST_TITLE, insertedList.get(0).getTitle());
+        Assert.assertEquals(TEST_TIMESTAMP, insertedList.get(0).getTimestamp());
+        Assert.assertEquals(TEST_CONTENT, insertedList.get(0).getContent());
+
+        note.setId(insertedList.get(0).getId());
+        Assert.assertEquals(note, insertedList.get(0));
+
+
+        //delete
+        insertedList = liveDataTestUtilList.getValue(getNoteDao().getNoteList());
+        Assert.assertEquals(0, insertedList.size());
+
+    }
+
+
+
 
 }
