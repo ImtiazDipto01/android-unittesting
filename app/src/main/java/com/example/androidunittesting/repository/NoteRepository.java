74be56b9a84610d9b5f96002a8c70e7be108221e
imtiaz <programmer.imtiaz@gmail.com>
2019-12-10 09:58:10 +0600

    ADD- Success and failure message added into NoteRepository

--- app/src/main/java/com/example/androidunittesting/repository/NoteRepository.java
+++ app/src/main/java/com/example/androidunittesting/repository/NoteRepository.java
@@ -3,13 +3,31 @@ package com.example.androidunittesting.repository;
 
 import androidx.annotation.NonNull;
 
+import com.example.androidunittesting.models.Note;
 import com.example.androidunittesting.room.NoteDao;
+import com.example.androidunittesting.utils.Resource;
+
+import java.util.concurrent.TimeUnit;
 
 import javax.inject.Inject;
 import javax.inject.Singleton;
 
+import io.reactivex.Flowable;
+
 @Singleton
 public class NoteRepository {
+
+    public static final String NOTE_TITLE_NULL = "Note title cannot be null";
+    public static final String INVALID_NOTE_ID = "Invalid id. Can't delete note";
+    public static final String DELETE_SUCCESS = "Delete success";
+    public static final String DELETE_FAILURE = "Delete failure";
+    public static final String UPDATE_SUCCESS = "Update success";
+    public static final String UPDATE_FAILURE = "Update failure";
+    public static final String INSERT_SUCCESS = "Insert success";
+    public static final String INSERT_FAILURE = "Insert failure";
+
+    private int timeDelay = 0;
+    private TimeUnit timeUnit = TimeUnit.SECONDS;
 
     @NonNull
     private NoteDao noteDao ;
@@ -18,4 +36,10 @@ public class NoteRepository {
     public NoteRepository(@NonNull NoteDao noteDao) {
         this.noteDao = noteDao;
     }
+
+    public Flowable<Resource<Integer>> insertNote(Note note){
+
+    }
+
+
 }
