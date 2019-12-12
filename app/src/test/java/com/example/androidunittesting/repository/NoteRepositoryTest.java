98c28d0394a900a4bc36a3d246b235061ffc91ce
imtiaz <programmer.imtiaz@gmail.com>
2019-12-12 09:00:59 +0600

    ADD- noteRepostiry not null objects checking test added into NoteRepositoryTest class

--- app/src/test/java/com/example/androidunittesting/repository/NoteRepositoryTest.java
+++ app/src/test/java/com/example/androidunittesting/repository/NoteRepositoryTest.java
@@ -1,7 +1,32 @@
 package com.example.androidunittesting.repository;
 
+import com.example.androidunittesting.room.NoteDao;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
+
 public class NoteRepositoryTest {
 
     private NoteRepository noteRepository ;
 
+    //@Mock
+    private NoteDao noteDao ;
+
+    @BeforeEach
+    public void initEach(){
+        //MockitoAnnotations.initMocks(this);
+        noteDao = Mockito.mock(NoteDao.class);
+        noteRepository = new NoteRepository(noteDao);
+
+    }
+
+    @Test
+    void checkObjectsNotNull() throws Exception {
+        Assertions.assertNotNull(noteDao);
+        Assertions.assertNotNull(noteRepository);
+    }
 }
